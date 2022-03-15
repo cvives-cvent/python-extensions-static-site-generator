57d429dbbcb304ee985b503ea4b249cf0eb6aba2
cvives-cvent <cvives@cvent.com>
2022-03-15 11:53:26 -0400

    module3

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from ssg import extensions
+from ssg import extensions, hooks
 
 class Site:
     def __init__(self, source, dest, parsers=None):
@@ -26,8 +26,10 @@ class Site:
                 "No parser for the {} extension, file skipped!".format(path.suffix)
             )
 
+
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files",self.source,self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
